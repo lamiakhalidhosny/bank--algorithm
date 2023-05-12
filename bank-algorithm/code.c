04d0d0fc6a64157b930a1e59559bbe310f2661bf
lamiakhalidhosny <96596374+lamiakhalidhosny@users.noreply.github.com>
2023-05-12 12:20:16 -0700

    Update code.c

--- bank-algorithm/code.c
+++ bank-algorithm/code.c
@@ -8,14 +8,14 @@ int main()
     m = 3; // Number of resources  
     int alloc[ 5 ] [ 3 ] = { { 0 , 1 , 0 }, // P0 // Allocation Matrix  
                         { 2 , 0 , 0 } , // P1  
-                        { 3 , 0 , 2 } , // P2  
-                        { 2 , 1 , 1 } , // P3  
-                        { 0 , 0 , 2 } } ; // P4  
+                        { 0 , 0 , 5 } , // P2  
+                        { 0 , 10 , 1 } , // P3  
+                        { 0 , 23 , 2 } } ; // P4  
     int max[ 5 ] [ 3 ] = { { 7 , 5 , 3 } , // P0 // MAX Matrix  
-                    { 3 , 2 , 2 } , // P1  
-                    { 9 , 0 , 2 } , // P2  
-                    { 2 , 2 , 2 } , // P3  
-                    { 4 , 3 , 3 } } ; // P4  
+                    { 1 , 2 , 9 } , // P1  
+                    { 2 , 0 , 8 } , // P2  
+                    { 3 , 0 , 7 } , // P3  
+                    { 4 , 0 , 6 } } ; // P4  
     int avail[3] = { 3 , 3 , 2 } ; // Available Resources  
     int f[n] , ans[n] , ind = 0 ;  
     for (k = 0; k < n; k++) {  
